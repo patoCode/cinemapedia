da8c08e9eee19f5001cb3f8cb204619613930577
Denis Rodriguez <denis_design@live.cl>
2023-12-13 17:49:43 -0400

    DIO-DATASOURCES Implementacion basica de Datasources con DIO

---
+++ lib/infraestructure/moviedb_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasources.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+class MovieDbDatasource extends MoviesDatasource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environment.theMovieDbKey,
+        'language': 'es-MX'
+      },
+    ),
+  );
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+    final List<Movie> movies = [];
+    return movies;
+  }
+}
